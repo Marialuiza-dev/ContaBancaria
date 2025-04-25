f55a543429a6131fa990a3c8d31da38071e2c46d
Maria Luiza <maluduartetorres@gmail.com>
2025-04-25 18:19:27 -0300

    :sparkles: criação da classe conta

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,6 +1,8 @@
 package conta;
 
 import java.util.Scanner;
+
+import conta.model.Conta;
 import conta.util.Cores;
 
 public class Menu {
@@ -8,6 +10,19 @@ public class Menu {
         Scanner leia = new Scanner(System.in);
         int opcao;
 
+        Conta c1 = new Conta(1, 123, 1, "Maria", 1000);
+        System.out.println(c1.getTitular());
+        c1.setTitular("Maria Luiza");
+        System.out.println(c1.getTitular());
+        
+        c1.visualizar();
+        
+        c1.sacar(1100);
+        c1.visualizar();
+        
+        c1.depositar(500);
+        c1.visualizar();
+        
         while (true) {
             System.out.println(Cores.TEXT_PURPLE_BOLD + Cores.ANSI_BLACK_BACKGROUND);
             System.out.println("*****************************************************");
